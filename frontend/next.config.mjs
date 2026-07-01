425c2b88882491dfc8489b8019f671c510078d79
ITCRC1 <it@thecostaricacollection.com>
2026-07-01 16:20:30 -0600

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   images: {
-    remotePatterns: [{ protocol: "http", hostname: "localhost" }],
+    remotePatterns: [
+      { protocol: "http",  hostname: "localhost" },
+      { protocol: "https", hostname: "sistema-vouchers-backend-production.up.railway.app" },
+    ],
   },
 };
 export default nextConfig;
